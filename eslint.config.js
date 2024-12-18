8b5bbb2c2a1e3e73a4283f6e408844459fbb75db
oshu <99813212+oshudev@users.noreply.github.com>
2024-12-18 13:31:45 +0800

    chore: default config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
 
 export default [
   { ignores: ['dist'] },
@@ -20,6 +21,7 @@ export default [
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
+      prettier,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -28,6 +30,7 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error',
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
@@ -35,4 +38,4 @@ export default [
       ],
     },
   },
-]
+];
