d6ada2d93a25f1a1c9b24e6cc8760c95f83e1eb2
oshu <99813212+oshudev@users.noreply.github.com>
2024-12-20 07:15:37 +0800

    feat: Add navigation bar

---
+++ src/sections/Header/Header.jsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from 'react';
+
+import { useMediaQuery } from '@uidotdev/usehooks';
+
+import ApplicationLogo from '@/components/ApplicationLogo';
+import { Button } from '@/components/Button';
+
+import styles from './Header.module.css';
+
+import navData from '@/data/nav.json';
+
+function Header() {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const isDesktop = useMediaQuery('only screen and (min-width: 1024px)');
+
+  useEffect(() => {
+    if (isDesktop && isMobileNavOpen) {
+      setIsMobileNavOpen(false);
+    }
+
+    const body = document.body;
+    const shouldScrollLock = !isDesktop && isMobileNavOpen;
+
+    body.classList.toggle('no-scroll', shouldScrollLock);
+
+    return () => body.classList.remove('no-scroll');
+  }, [isDesktop, isMobileNavOpen]);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className="container">
+          <a
+            href="#"
+            className={styles.header__logo}
+            aria-label="Go to Shortly homepage"
+          >
+            <ApplicationLogo />
+          </a>
+          <nav className={styles.header__nav} aria-hidden={!isDesktop}>
+            {navData.map((item, index) => (
+              <ul className={styles.header__list} key={index}>
+                <li className={styles.header__item}>
+                  <a href={item.link}>{item.label}</a>
+                </li>
+              </ul>
+            ))}
+          </nav>
+          <a href="#" className="login" aria-hidden={!isDesktop}>
+            Login
+          </a>
+          <Button
+            className="signup"
+            variant="rounded"
+            size="sm"
+            aria-hidden={!isDesktop}
+            asChild
+          >
+            <a href="#">Sign Up</a>
+          </Button>
+          <button
+            onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
+            className={`${styles.hamburger} ${isMobileNavOpen ? styles.active : ''}`}
+            aria-hidden={isDesktop}
+          >
+            <span></span>
+          </button>
+        </div>
+      </header>
+      {isMobileNavOpen && (
+        <div className={styles['mobile-modal-nav']}>
+          <nav className={styles.modal__nav}>
+            {navData.map((item, index) => (
+              <a href={item.link} key={index}>
+                {item.label}
+              </a>
+            ))}
+          </nav>
+          <a href="#" className="login">
+            Login
+          </a>
+          <Button variant="rounded" size="md" asChild>
+            <a href="#">Sign Up</a>
+          </Button>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Header;
