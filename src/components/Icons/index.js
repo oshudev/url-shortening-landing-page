a62e195fc69563ddd0e13627bda8f7b538f66aab
oshu <99813212+oshudev@users.noreply.github.com>
2024-12-21 12:43:26 +0800

    feat: Add footer contact row

---
+++ src/components/Icons/index.js
@@ -0,0 +1,4 @@
+export { default as FacebookIcon } from './Facebook';
+export { default as TwitterIcon } from './Twitter';
+export { default as PinterestIcon } from './Pinterest';
+export { default as InstagramIcon } from './Instagram';
--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,5 +1,15 @@
-import { Button } from "@/components/Button";
-import styles from "./Footer.module.css";
+import ApplicationLogo from '@/components/ApplicationLogo';
+import { Button } from '@/components/Button';
+import {
+  FacebookIcon,
+  InstagramIcon,
+  PinterestIcon,
+  TwitterIcon,
+} from '@/components/Icons';
+
+import styles from './Footer.module.css';
+
+import navData from '@/data/footer.json';
 
 function Footer() {
   return (
@@ -9,6 +19,45 @@ function Footer() {
         <Button variant="rounded" asChild>
           <a href="#">Get Started</a>
         </Button>
+      </div>
+      <div className={styles['contact-row']}>
+        <div className="container">
+          <a
+            href="#"
+            className={styles.footer__logo}
+            aria-label="Go to Shortly homepage"
+          >
+            <ApplicationLogo variant="light" />
+          </a>
+          <nav className={styles.footer__nav} aria-label="Footer nav">
+            {navData.map(({ header, links }) => (
+              <div className={styles['footer-nav-container']} key={header}>
+                <p>{header}</p>
+                <ul className={styles.footer__list}>
+                  {links.map((item, index) => (
+                    <li className={styles.footer__item} key={index}>
+                      <a href={item.link}>{item.label}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </nav>
+          <div className={styles.footer__social}>
+            <a href="#">
+              <FacebookIcon />
+            </a>
+            <a href="#">
+              <TwitterIcon />
+            </a>
+            <a href="#">
+              <PinterestIcon />
+            </a>
+            <a href="#">
+              <InstagramIcon />
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
